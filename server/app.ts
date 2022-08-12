2f0e8df1d0517b972369325fafc042758626c9a7
DevelopmentCJ <development@clinicacruzjiminian.com>
2022-08-12 16:06:44 -0400

    Fixing Vue Router

--- server/app.ts
+++ server/app.ts
@@ -1,4 +1,5 @@
 import express from "express";
+var history = require("connect-history-api-fallback");
 import morgan from "morgan";
 import cors from "cors";
 import path from "path";
@@ -9,7 +10,17 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
+//Fix CannotGet Heroku Vue Router
+app.use(
+  history({
+    // OPTIONAL: Includes more verbose logging
+    verbose: true,
+  })
+);
+
 // read static files
 app.use(express.static(path.join(__dirname, "..", "..", "dist")));
-app.get("*", (req, res) => {res.sendFile(path.join(__dirname, "..", "..", "dist"));});
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "..", "..", "dist"));
+});
 export default app;
